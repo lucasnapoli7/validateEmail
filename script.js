05a8514230480e72a7ac7b6a2356d624a66a835d
Lucas Nápoli <lucasalannapoli@gmail.com>
2021-02-03 15:12:02 -0300

    Visual update

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const button = document.getElementById('button')
 
 function validate(email) {
-    if(email.match(/^[^@]+@[\w\.]{2,10}\.[\w]{2,5}$/i)) {
-    button.disabled = false
-    button.classList.remove('disabled')
+    if (email.match(/^[^@]+@[\w\.]{2,10}\.[\w]{2,5}$/i)) {
+        button.disabled = false
+        button.classList.remove('disabled')
     } else {
-    button.disabled = true
-    button.classList.add('disabled')
+        button.disabled = true
+        button.classList.add('disabled')
     }
 }
 
 function correct() {
     swal({
-    title: 'Congratulations!',
-    text: 'This is a correct Email',
-    icon: 'success',
-    button: 'Close'
+        title: 'Congratulations!',
+        text: 'This is a correct Email',
+        icon: 'success',
+        button: 'Close'
     })
 }
